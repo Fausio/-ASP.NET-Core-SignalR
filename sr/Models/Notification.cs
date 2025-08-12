918fdfa890c0a47d65e321b8e446bba5463f406a
fausio luis <fausioluis@live.com>
2025-08-12 17:08:10 +0200

    return btn

--- sr/Models/Notification.cs
+++ sr/Models/Notification.cs
@@ -6,8 +6,5 @@
         public string Text { get; set; }
         public string url { get; set; }
         public List<NotificationApplicationUser> NotificationApplicationUsers { get; set; }
-    }
-
-
-
+    } 
 }
